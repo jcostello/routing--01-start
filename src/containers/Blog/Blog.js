eaa0c27dca29d28173b67b42ff75acd3766df808
Juan Manuel Costello <juan.costello@altoros.com>
2019-11-20 18:04:38 -0300

    First

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom'
+
+import Posts from './../Posts/Posts'
+import NewPost from './../NewPost/NewPost'
+import FullPost from './../FullPost/FullPost'
+import './Blog.css';
+
+class Blog extends Component {
+  render() {
+    return (
+      <div className='Blog'>
+        <header>
+          <nav>
+            <ul>
+              <li><NavLink to='/' exact>Home</NavLink></li>
+              <li><NavLink to='/posts/new'>New Post</NavLink></li>
+            </ul>
+          </nav>
+        </header>
+
+        <Switch>
+          <Route path='/' exact component={Posts} />
+          <Route path='/posts/new' exact component={NewPost} />
+          <Route path='/posts/:id' exact component={FullPost} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default Blog;
---
+++ src/containers/Posts/Posts.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from '../../axios'
+
+import Post from './../../components/Post/Post'
+import './Posts.css';
+
+class Posts extends Component {
+  state = {
+    posts: [],
+  }
+
+  componentDidMount() {
+    axios.get('/posts')
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Max'
+          }
+        });
+
+        this.setState({ posts: updatedPosts });
+      })
+      .catch(error => { });
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  }
+
+  render() {
+    let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+
+        return (
+          <Link to={'/posts/' + post.id} key={post.id}>
+            <Post
+              title={post.title}
+              author={post.author}
+              clicked={() => this.postSelectedHandler(post.id)} />
+          </Link>
+        )
+      });
+    }
+
+    return (
+      <section className="Posts">
+        {posts}
+      </section>
+    )
+  }
+}
+
+export default Posts;
